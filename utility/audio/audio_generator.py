d71ed5f290669a0488e76a23dbb115059ea13d32
justCool-glitch <shreyanarayan833@gmail.com>
2024-11-06 17:31:27 +0545

    print prompt

--- utility/audio/audio_generator.py
+++ utility/audio/audio_generator.py
@@ -1,7 +1,7 @@
 import edge_tts
 
 async def generate_audio(text,outputFilename):
-    communicate = edge_tts.Communicate(text,"en-AU-WilliamNeural")
+    communicate = edge_tts.Communicate(text=text,voice="en-AU-WilliamNeural",te="-20%",volume= "-10%",pitch="-10Hz")
     await communicate.save(outputFilename)
 
 
